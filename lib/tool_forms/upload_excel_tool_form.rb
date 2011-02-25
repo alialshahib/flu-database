949a1cf0cda2bd5b62ba58d226754ea22012381b
Paul-Michael Agapow <pma+github@agapow.net>
2011-02-25 17:47:27 +0000

    file upload file types

--- lib/tool_forms/upload_excel_tool_form.rb
+++ lib/tool_forms/upload_excel_tool_form.rb
@@ -39,6 +39,22 @@ module ToolForms
 			pp params["spreadsheet"].methods.sort
 			pp "**** path #{params['spreadsheet'].original_filename}"
 			
+			sheet_file = params['spreadsheet']
+			sheet_file_name = sheet_file.original_filename
+			file_ext = sheet_file_name.original_filename[/\.\w+$/]
+			if file_ext.nil?
+				return [], ["can't determine file type of '#{sheet_file_name}'"]
+			end
+			file_ext = file_ext[1,x.size].downcase()
+			if ! ['xls', 'xlsx'].member?(file_ext)
+				return [], ["can't handle files of type '#{file_ext}'"]
+			end
+			
+			rdr = SpreadsheetReader::ExcelReader.new(sheet_file, :file_type => file_ext)
+			rdr.read() { |rec|
+				pp rec
+			}
+
 			# NOTE: returns results & errors
 			return ["foo", 127], []
 		end
