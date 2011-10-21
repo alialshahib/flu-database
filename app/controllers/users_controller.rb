108ceaa6372306a009c27d0f3d69cf744160b440
Paul-Michael Agapow <pma+github@agapow.net>
2011-10-21 18:18:54 +0100

    repairing & simplifying

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,16 @@
 class UsersController < ApplicationController
 
-  hobo_user_controller
+	hobo_user_controller
+	
+	auto_actions(:all)  #, :except => [:view, :new, :create ] 
+	
+	autocomplete
 
-  auto_actions(:all)  #, :except => [:view, :new, :create ] 
-  
-#  :except => [ :index, :new, :create ]
-
-  autocomplete
-
-  def do_signup
-     hobo_do_signup do
-       if this.errors.blank?
-         flash[:notice] << "You must activate your account before you can log in.  Please check your email."
-        end
-     end
-  end
+	def do_signup
+		hobo_do_signup do
+			if this.errors.blank?
+				flash[:notice] << "You must activate your account before you can log in.  Please check your email."
+			end
+		end
+	end
 end
