af09a4fbf196f2c5d0547f8c53583c5e01dcb245
Paul-Michael Agapow <pma+github@agapow.net>
2011-10-26 16:17:22 +0100

    added new logo and user levels

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,7 +1,13 @@
+
+# A period or interval within which a threshold holds or a report is made
+# 
+# Seasons begin at the same day every year, so only the year need be recorded.
+# 
 class Season < ActiveRecord::Base
-   # TODO: needs a method to find current season?
-   # TODO: need ordering in display
-   
+	# TODO: when does Southern hemisphere season start?
+	# TODO: name the seasons better?
+	# TODO: year field description
+
 	hobo_model # Don't put anything above this
 
 	fields do
@@ -11,11 +17,14 @@ class Season < ActiveRecord::Base
 
 	has_many :thresholds
 
-   set_default_order "year DESC"
+	set_default_order "year DESC"
+
+	# when does a season start?
+	# TODO: this should really be defined globally
+	START_MONTH = 1
+	START_DAY = 1
 
 	## Permissions:
-	# XXX: seasons can only be created by adminstrators. Seem right?
-	
 	def create_permitted?
 		acting_user.administrator?
 	end
@@ -31,9 +40,9 @@ class Season < ActiveRecord::Base
 	def view_permitted?(field)
 		true
 	end
-	
+
 	## Accessors:
-	
+
 	# Return the name of the season.
 	#
 	# This will be based on the starting year.
@@ -41,36 +50,51 @@ class Season < ActiveRecord::Base
 	def name
 		return "#{year} season"
 	end
-	
+
 	# What is the first day of the season?
 	#
-	# Currently we assume this is the first day of the year.
-	#
 	def start_date
-		return Date.new(y=year, m=1, d=1)
+		return Date.new(y=year, m=START_MONTH, d=START_DAY)
 	end
 
 	# What is the last day of the season?
 	#
-	# Currently we assume this is the last day of the year.
-	#	
 	def stop_date
-		return Date.new(y=year, m=12, d=31)
+		#return Date.new(y=year+1, m=START_MONTH, d=START_DAY) - 1
+		return start_date.next_year.prev_day()
 	end
-	
+
 	# Does a day fall within this season?
 	#
 	# We judge this based on the first and last days of a season.
 	#
 	def date_within(d)
-      return (start_date <= d) && (d <= stop_date)
+		return (start_date <= d) && (d <= stop_date)
 	end
 
-	# Fetch the current season.
+	# Return the name of the season.
 	#
-	def self.find_current_season
-      current_year = Date.now.year
-      return year_is(current_year)
+	# This will be based on the starting year.
+	#
+	def description
+		return "Running from #{start_date} to #{stop_date}"
 	end
 	
+	# Fetch the current season.
+	# 
+	# Returns: the record that "contains" the current date. If none fits,
+	#    return nil
+	#
+	def self.find_current_season
+		# TODO: does this work?
+		current_date = Date.now()
+		return self.all { |s|
+			if s.date_within(current_date)
+				return s
+			end
+		}
+		
+		return nil
+	end
+
 end
--- app/models/user_country.rb
+++ app/models/user_country.rb
@@ -1,35 +1,44 @@
+
 # A join table for the many-to-many relationship between users & countries.
 #
+#
 class UserCountry < ActiveRecord::Base
-   
-   hobo_model # Don't put anything above this
-   
-   ## Fields & relationships:
-   # TODO: may need to set levels of access / user level for each country, such
-   #   as can view (reader), can add & edit seasons (editor), can add members
-   # and edit country data (manager). Also add thresholds?
-   fields do
-      timestamps
-   end
-   
-   belongs_to :user
-   belongs_to :country
-   
-   ## Permissions:
-   def create_permitted?
-      acting_user.administrator?
-   end
-   
-   def update_permitted?
-      acting_user.administrator?
-   end
-   
-   def destroy_permitted?
-      acting_user.administrator?
-   end
-   
-   def view_permitted?(field)
-      true
-   end
+
+	hobo_model # Don't put anything above this
+
+	## Defines & Constants:
+	
+	# what can the user do with the country?
+	Level = HoboFields::EnumString.for(:reader, :editor, :manager)
+	
+	## Fields & relationships:
+	# TODO: may need to set levels of access / user level for each country, such
+	#   as can view (reader), can add & edit seasons (editor), can add members
+	# and edit country data (manager). Also add thresholds?
+	fields do
+		level         UserCountry::Status, :required, :default => :editor
+		
+		timestamps
+	end
+
+	belongs_to :user
+	belongs_to :country
+
+	## Permissions:
+	def create_permitted?
+		acting_user.administrator?
+	end
+
+	def update_permitted?
+		acting_user.administrator?
+	end
+
+	def destroy_permitted?
+		acting_user.administrator?
+	end
+
+	def view_permitted?(field)
+		true
+	end
 
 end
